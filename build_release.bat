0c099d7219f393f30e27c271adc7aac4065b7db1
baluoteliz <meixi@live.cn>
2019-10-16 22:30:08 +0800

    update build_release.bat

--- build_release.bat
+++ build_release.bat
@@ -1,12 +1,11 @@
 @echo off
 title qmake and nmake build prompt
-set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
-set QTDIR=C:\Qt\Qt5.6.2\5.6\msvc2013
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+set QTDIR=C:\Qt\5.12.2\msvc2017
 set PATH=%VCINSTALLDIR%\bin;%QTDIR%\bin;C:\Program Files\7-Zip;%PATH%
 ::shadow build
-call "%VCINSTALLDIR%\vcvarsall.bat"
-qmake OpenVideoCall.pro -spec win32-msvc2013 "CONFIG+=release" "CONFIG+=qml_release"
+call "%VCINSTALLDIR%\vcvarsall.bat" x86
+qmake OpenVideoCall.pro "CONFIG+=release" "CONFIG+=qml_release"
 nmake
 cd release
 windeployqt OpenVideoCall.exe
-OpenVideoCall.exe
